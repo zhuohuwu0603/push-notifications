9ab14cb1ee0de71e0b0cad679e9f2e482c8f4751
Kevin Wu <kevin@viafoura.com>
2016-08-24 11:42:52 -0400

    Initial commit

--- app/sw.js
+++ app/sw.js
@@ -22,3 +22,22 @@
 'use strict';
 
 // TODO
+console.log('Started', self);
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+  console.log('Installed', event);
+});
+self.addEventListener('activate', function(event) {
+  console.log('Activated', event);
+});
+self.addEventListener('push', function(event) {
+  console.log('Push message', event);
+
+  var title = 'Push message-title';
+
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      'body': 'The Message-body2',
+      'icon': 'images/icon.png'
+    }));
+});
