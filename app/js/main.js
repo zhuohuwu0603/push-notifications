3546feb0be9dbd7c04bf24d2db4580f4771409bd
Kevin Wu <kevin@viafoura.com>
2016-08-24 13:39:11 -0400

    print auth and p256dh
    ie: Subscribed! Endpoint stringify: {"endpoint":"https://android.googleapis.com/gcm/send/fvTQzr9DFQM:APA91bHdgHWrGNDwBv54g3g…SCT_ZPXnIMYSNIqihN-onlpuonKdxpIs-ooIEWscPbIzYKX_e_MGIf4vzoOy7ljgtRjjOnpVAr","keys":{"p256dh":"BFibu83wALJYOqCWIzSKK5Ak6qSHcievsHZEsQv8sfLPPMVJC3oQp9ur64oHOl3mpplZEpNKHnXgPvPspCHclZM=","auth":"NAjTEJAvuwMPM_ihAmIl-g=="}}

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,55 @@
 'use strict';
 
 // TODO
+
+var reg;
+var sub;
+var isSubscribed = false;
+var subscribeButton = document.querySelector('button');
+
 if ('serviceWorker' in navigator) {
   console.log('Service Worker is supported');
   navigator.serviceWorker.register('sw.js').then(function() {
     return navigator.serviceWorker.ready;
-  }).then(function(reg) {
+  }).then(function(serviceWorkerRegistration) {
+    reg = serviceWorkerRegistration;
+    subscribeButton.disabled = false;
     console.log('Service Worker is ready :^)', reg);
-    reg.pushManager.subscribe({userVisibleOnly: true}).then(function(sub) {
-      console.log('endpoint:', sub.endpoint);
-    });
   }).catch(function(error) {
-    console.log('Service Worker error :^(', error);
+    console.log('Service Worker Error :^(', error);
+  });
+}
+
+subscribeButton.addEventListener('click', function() {
+  if (isSubscribed) {
+    unsubscribe();
+  } else {
+    subscribe();
+  }
+});
+
+function subscribe() {
+  reg.pushManager.subscribe({userVisibleOnly: true}).
+  then(function(pushSubscription) {
+    sub = pushSubscription;
+    console.log('Subscribed! Endpoint:', sub.endpoint);
+    console.log('Subscribed! Endpoint stringify:', JSON.stringify(sub));
+    console.log('Subscribed! Endpoint100:', sub.getKey('auth'));
+    console.log('Subscribed! Endpoint101:', new Uint8Array(sub.getKey('auth')));
+    console.log('Subscribed! Endpoint200:', sub.getKey('p256dh'));
+    console.log('Subscribed! Endpoint201:', new Uint8Array(sub.getKey('p256dh')));
+    subscribeButton.textContent = 'Unsubscribe';
+    //ßisSubscribed = true;
+  });
+}
+
+function unsubscribe() {
+  sub.unsubscribe().then(function(event) {
+    subscribeButton.textContent = 'Subscribe';
+    console.log('Unsubscribed!', event);
+    isSubscribed = false;
+  }).catch(function(error) {
+    console.log('Error unsubscribing', error);
+    subscribeButton.textContent = 'Subscribe';
   });
 }
